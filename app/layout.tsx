2f587894e09cf512b4171caa785ef76f21150cdd
ammar <ammarkhan575@gmail.com>
2024-01-25 02:56:13 +0530

    added assets and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,32 @@
-import { ClerkProvider } from '@clerk/nextjs'
-import './globals.css'
 import React from 'react'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+// eslint-disable-next-line
+import {Inter, Space_Grotesk} from 'next/font/google'
+import type { Metadata } from 'next'
+
+export const metadat : Metadata ={
+  title: 'Dev-Overflow',
+  description: 'A platform where you can Get help and share knowlege with others, and updated with latest technology (Web Development, Mobile Development, Game Development, etc)',
+  icons: '/assets/images/site-logo.png'
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  weight:[
+    '100', '200', '300', '400', '500', '600', '700',
+    '800', '900'
+  ],
+  variable: '--font-inter'
+})
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight:[
+    '300', '400', '500', '600', '700'
+  ],
+  variable: '--font-spaceGrotesk'
+})
 
 export default function RootLayout({
   children,
@@ -8,9 +34,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        elements:{
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500'
+        }
+      }}
+    >
       <html lang="en">
-        <body>{children}</body>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   )
