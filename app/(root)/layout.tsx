1e9fa72a707b1565a443007ef02eefdb65027c88
ammar <ammarkhan575@gmail.com>
2024-01-26 00:40:30 +0530

    rightSidebar completed

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "@/components/shared/LeftSidebar/LeftSidebar";
+import RightSideBar from "@/components/shared/RightSidebar/RightSideBar";
 import Navbar from "@/components/shared/navbar/Navbar";
 import React from "react";
 
@@ -11,6 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <section className="flex min-h-screen flex-1 flex-col px-6 pt-36 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
+        <RightSideBar/>
       </div>
     </main>
   );
